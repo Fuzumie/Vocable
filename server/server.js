54213ed53a0a24d9ea86a5cb19933545ea863dc4
Fuzumie <Oxyd456852@gmail.com>
2024-03-28 19:20:25 +0100

    idk

--- server/server.js
+++ server/server.js
@@ -1,16 +1,29 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mysql = require('mysql')
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = 8000;
-app.use(cors())
+const mongoose = require('mongoose');
+app.use(cors());
 
-const db= mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    
-})
+
+const uri = "mongodb+srv://Boxin:e3wqrd@vocabledata.hqzobhy.mongodb.net/?retryWrites=true&w=majority&appName=vocableData";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, ()=>{
-    console.log('listening')
+    console.log('listening');
 })
