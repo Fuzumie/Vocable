6b18a0fc55afb711fa14041468f4e4adbc88225f
Fuzumie <Oxyd456852@gmail.com>
2024-05-05 22:28:12 +0200

    css

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useAuthContext } from '../hooks/useAuthContext';
-import { useVocabsContext } from '../hooks/useVocabsContext';
-import VocabDetails, {handleCreateVocabulary} from '../components/VocabDetails';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useVocabsContext } from "../hooks/useVocabsContext";
+import VocabDetails, {
+  handleCreateVocabulary,
+} from "../components/VocabDetails";
 
 const Dashboard = () => {
   const [userInfo, setUserInfo] = useState(null);
   const { vocabs, dispatch } = useVocabsContext();
   const { user } = useAuthContext();
   const token = user.token;
-  const [newVocabularyName, setNewVocabularyName] = useState('');
+  const [newVocabularyName, setNewVocabularyName] = useState("");
   const [showModal, setShowModal] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
-  
+  const [searchQuery, setSearchQuery] = useState("");
+
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
-        const response = await axios.get('/api/user', {
+        const response = await axios.get("/api/user", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
         setUserInfo(response.data);
       } catch (error) {
-        console.error('Error fetching user information:', error);
+        console.error("Error fetching user information:", error);
       }
     };
 
@@ -33,12 +35,12 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchVocabs = async () => {
       try {
-        const response = await axios.get('/api/vocab', {
+        const response = await axios.get("/api/vocab", {
           headers: { Authorization: `Bearer ${token}` },
         });
-        dispatch({ type: 'SET_VOCABS', payload: response.data });
+        dispatch({ type: "SET_VOCABS", payload: response.data });
       } catch (error) {
-        console.error('Error fetching vocabularies:', error);
+        console.error("Error fetching vocabularies:", error);
       }
     };
 
@@ -46,9 +48,15 @@ const Dashboard = () => {
       fetchVocabs();
     }
   }, [dispatch, token, user]);
-  
+
   const createVocabulary = async () => {
-    await handleCreateVocabulary(newVocabularyName, token, dispatch, setShowModal, setNewVocabularyName);
+    await handleCreateVocabulary(
+      newVocabularyName,
+      token,
+      dispatch,
+      setShowModal,
+      setNewVocabularyName
+    );
   };
 
   const calculateWinRate = () => {
@@ -58,19 +66,23 @@ const Dashboard = () => {
     }
     return null; // Return null if user has not played a single game
   };
-  const filteredResults = vocabs && vocabs.filter(vocab => vocab.name.toLowerCase().includes(searchQuery.toLowerCase()));
+  const filteredResults =
+    vocabs &&
+    vocabs.filter((vocab) =>
+      vocab.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
   return (
-    <div className='dashboard'>
-      <h1 className='stats-h1'>Dashboard</h1>
+    <div className="dashboard">
+      <h1 className="stats-h1">Dashboard</h1>
       {userInfo && (
-        <div className='stats'>
+        <div className="stats">
           <p>Games Played: {userInfo.games}</p>
           <p>Wins: {userInfo.wins}</p>
           <p>Loses: {userInfo.loses}</p>
           {userInfo && userInfo.games > 0 && (
-          <p>Win Rate: {calculateWinRate()}%</p>
-      )}
-      </div>
+            <p>Win Rate: {calculateWinRate()}%</p>
+          )}
+        </div>
       )}
       <div>
         <input
@@ -81,31 +93,39 @@ const Dashboard = () => {
         />
         <i class="fa-solid fa-magnifying-glass"></i>
       </div>
-      
-      <button className='create-vocab-button' onClick={() => setShowModal(true)}>Create Vocabulary</button>
-      
-      <div className='vocab'>
-       <div className='vocab-body'>  
-      {filteredResults && filteredResults.map((result) => (
-          <VocabDetails key={result._id} vocabulary={result} />
-        ))}
-      </div> 
-      
-      {showModal && (
-        <div className="modal-dashboard">
-          <div className="modal-dashboard-content">
-            <span className="close" onClick={() => setShowModal(false)}>&times;</span>
-            <h2>Create Vocabulary</h2>
-            <input
-              type="text"
-              value={newVocabularyName}
-              onChange={(e) => setNewVocabularyName(e.target.value)}
-              placeholder="New Vocabulary Name"
-            />
-            <button onClick={createVocabulary}>Create</button>
-          </div>
+
+      <button
+        className="create-vocab-button"
+        onClick={() => setShowModal(true)}
+      >
+        Create Vocabulary
+      </button>
+
+      <div className="vocab">
+        <div className="vocab-body">
+          {filteredResults &&
+            filteredResults.map((result) => (
+              <VocabDetails key={result._id} vocabulary={result} />
+            ))}
         </div>
-      )}
+
+        {showModal && (
+          <div className="modal-dashboard">
+            <div className="modal-dashboard-content">
+              <span className="close" onClick={() => setShowModal(false)}>
+                &times;
+              </span>
+              <h2>Create Vocabulary</h2>
+              <input
+                type="text"
+                value={newVocabularyName}
+                onChange={(e) => setNewVocabularyName(e.target.value)}
+                placeholder="New Vocabulary Name"
+              />
+              <button onClick={createVocabulary}>Create</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
